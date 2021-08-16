094c76f6393c6a0fa15e78fd15e465d75a25944f
FlashOn <balaji.mani1607@gmail.com>
2021-08-17 00:11:43 +1000

    Add files via upload

---
+++ sql.js
@@ -0,0 +1,30 @@
+const mysql = require("mysql");
+const dbSocketPath = process.env.DB_SOCKET_PATH || "/cloudsql";
+const connection = {
+    connectionLimit: 10,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+};
+console.log('connection', connection);
+const pool = mysql.createPool(connection);
+class Repository {
+    async prepareQuery(query, parameters) {
+        console.log("----------------------------prepareQuery called-----------------------------");
+        return new Promise((resolve, reject) => {
+            pool.getConnection(function (err, connection) {
+                connection.query(query, parameters, (err, result) => {
+                    if (err) {
+                        console.log("-------------------- while prepare query errrrrrrrr------------------------------>", err);
+                        reject(err);
+                    };
+                    connection.release();
+                    resolve(result);
+                });
+            });
+        });
+    }
+};
+exports.Repository = Repository;
